125745dfb8e4a54ff69471d7f668db36e5c98e14
Ulrich Büttgen <Ulrich.Buettgen@businessacts.de>
2015-06-23 13:48:26 +0200

    Added jetty und extend SampleWebApp example with UIField binding.

--- src/main/java/org/gwtbootstrap3/sample/client/SampleWebApp.java
+++ src/main/java/org/gwtbootstrap3/sample/client/SampleWebApp.java
@@ -1,8 +1,11 @@
 package org.gwtbootstrap3.sample.client;
 
+import org.gwtbootstrap3.client.ui.Heading;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -11,12 +14,16 @@ import com.google.gwt.user.client.ui.Widget;
  * @author Joshua Godi
  */
 public class SampleWebApp extends Composite implements EntryPoint {
-    interface MyUiBinder extends UiBinder<Widget, SampleWebApp> {
-    }
+	interface MyUiBinder extends UiBinder<Widget, SampleWebApp> {
+	}
 
-    private static MyUiBinder myUiBinder = GWT.create(MyUiBinder.class);
+	private static MyUiBinder myUiBinder = GWT.create(MyUiBinder.class);
 
-    public void onModuleLoad() {
-        RootPanel.get().add(myUiBinder.createAndBindUi(this));
-    }
+	@UiField
+	Heading jumboHeader;
+
+	public void onModuleLoad() {
+		RootPanel.get().add(myUiBinder.createAndBindUi(this));
+		jumboHeader.setText("Hello World!");
+	}
 }
